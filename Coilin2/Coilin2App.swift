ac632b8619804e558f0bb449bee67f72507d425c
famikeeki <fami53977@gmail.com>
2025-07-01 10:37:09 +0900

    Initial Commit

---
+++ Coilin2/Coilin2App.swift
@@ -0,0 +1,17 @@
+//
+//  Coilin2App.swift
+//  Coilin2
+//
+//  Created by 桜田聖和 on 2025/07/01.
+//
+
+import SwiftUI
+
+@main
+struct Coilin2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coilin2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Coilin2
+//
+//  Created by 桜田聖和 on 2025/07/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
